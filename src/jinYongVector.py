59695009f5a72d7c318d677601283d772e2244f9
liuning11 <liuning11@jd.com>
2020-01-07 13:22:09 +0800

    fixed

--- src/jinYongVector.py
+++ src/jinYongVector.py
@@ -1,5 +1,8 @@
 import jieba
-import gensim.models.word2vec as w2v
+# import gensim.models.word2vec as w2v
+# import gensim.models.word2vec as w2v
+from gensim.test.utils import common_texts
+from gensim.models import KeyedVectors, Word2Vec
 
 # file_path = '../txt/jinYong.txt'
 
@@ -17,10 +20,28 @@ import gensim.models.word2vec as w2v
 #     line = fin.readline()
 # fin.close()
 # fou.close()
+for t in common_texts:
+    print(t)
 
-file_segment_path = '../txt/jinYong_segment.txt'
+# model = Word2Vec(common_texts, size=100, window=5, min_count=1, workers=4)
+# word_vectors = model.wv
+
+
+
+
+
+
+#
+# file_segment_path = '../txt/jinYong_segment.txt'
 # model_file_name = '../txt/jinYong_vector.txt'
-sentences = w2v.LineSentence(file_segment_path)
-model = w2v.Word2Vec(sentences, size=20, window=5, min_count=5, workers=4)
-s = model.most_similar('张三丰')
-print(s)
+# # sentences = w2v.LineSentence(file_segment_path)
+#
+# word_vectors = KeyedVectors.load(model_file_name, mmap='r')
+#
+# # word_vectors = KeyedVectors.load_word2vec_format(datapath('word2vec_pre_kv_c'), binary=False)
+#
+# # model = w2v.Word2Vec(sentences, size=20, window=5, min_count=5, workers=4)
+# s = word_vectors.most_similar('张君宝')
+#
+# # gensim.models.keyedvectors.WordEmbeddingsKeyedVectors.most_similar
+# print(s)
